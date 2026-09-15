61ef611f2e51ae4d558553794320f3ca00cd5f81
AsherHsu <hsushuhao127@gmail.com>
2026-09-16 00:14:56 +0800

    feat(v9.9.4): assemble generated menu key art

---
+++ source/src/data/MenuArtV994.js
@@ -0,0 +1,4 @@
+import { MENU_ART_V994_0 } from './MenuArtV994Chunk0.js';
+import { MENU_ART_V994_1 } from './MenuArtV994Chunk1.js';
+
+export const MENU_KEYART_V994 = 'data:image/jpeg;base64,' + MENU_ART_V994_0 + MENU_ART_V994_1;
